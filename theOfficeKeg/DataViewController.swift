3203338f4f9c0017ea940eac760e947ee804843e
Shawn Campbell <scampbell@vetsfirstchoice.com>
2015-06-14 14:48:42 -0400

    Initial Commit

---
+++ theOfficeKeg/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  theOfficeKeg
+//
+//  Created by Shawn Campbell on 6/14/15.
+//  Copyright © 2015 Vet's First Choice. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
